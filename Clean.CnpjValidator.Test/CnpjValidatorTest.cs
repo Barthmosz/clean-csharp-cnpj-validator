da4a016f3ac8b602f4a5837bf1c089aae54d6a3c
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-05-05 22:46:44 -0300

    test: ensure CnpjValidator returns false if an invalid cnpj is provided

---
+++ Clean.CnpjValidator.Test/CnpjValidatorTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Clean.CnpjValidator.Test
+{
+    [TestFixture]
+    public class CnpjValidatorTest
+    {
+        [Test, Description("ensure CnpjValidator returns false if an invalid cnpj is provided")]
+        public void CnpjValidator_InvalidCnpj_ReturnsFalse()
+        {
+            Assert.That(CnpjValidator.Validate("12.345.678/9123"), Is.EqualTo(false));
+        }
+    }
+}
