c79235615c13db003e09939dcea73e67b052b71b
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-05-05 22:48:20 -0300

    docs: add docstring to CnpjValidator

--- Clean.CnpjValidator/CnpjValidator.cs
+++ Clean.CnpjValidator/CnpjValidator.cs
@@ -4,6 +4,11 @@ namespace Clean.CnpjValidator
 {
     public static class CnpjValidator
     {
+        /// <summary>
+        /// Validates a cnpj.
+        /// </summary>
+        /// <param name="cnpj"></param>
+        /// <returns></returns>
         public static bool Validate(string cnpj)
         {
             string cnpjRegex = "^\\d{2}\\.\\d{3}\\.\\d{3}\\/\\d{4}-\\d{2}$";
