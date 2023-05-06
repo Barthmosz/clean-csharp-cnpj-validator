498db30082141bc68c2167706d37d533f17ad471
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-05-05 22:46:33 -0300

    refactor: change CnpjValidator method name

--- Clean.CnpjValidator/CnpjValidator.cs
+++ Clean.CnpjValidator/CnpjValidator.cs
@@ -4,7 +4,7 @@ namespace Clean.CnpjValidator
 {
     public static class CnpjValidator
     {
-        public static bool Validator(string cnpj)
+        public static bool Validate(string cnpj)
         {
             string cnpjRegex = "^\\d{2}\\.\\d{3}\\.\\d{3}\\/\\d{4}-\\d{2}$";
             return Regex.IsMatch(cnpj, cnpjRegex);
